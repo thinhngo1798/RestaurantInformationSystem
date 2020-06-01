604af83f1ac649ac80f9d466d19840001b9f37ce
Thinh <thinhngo.1798@gmail.com>
2020-06-01 13:19:36 +1000

    Processing ordering interface

---
+++ RestaurantInformationSystem/RestaurantInformationSystem/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantInformationSystem
+{
+    public class Menu
+    {
+        private List<MenuItem> _menuList;
+
+        public Menu()
+        {
+            MenuItem item1 = new MenuItem(1, "chicken", 3.0, 5);
+            MenuItem item2 = new MenuItem(1, "pork", 5.0, 10);
+            MenuItem item3 = new MenuItem(1, "beff", 6.0, 7);
+            MenuList.Add(item1);
+            MenuList.Add(item2);
+            MenuList.Add(item3);
+        }
+
+        internal List<MenuItem> MenuList { get => _menuList; set => _menuList = value; }
+    }
+}
---
+++ RestaurantInformationSystem/RestaurantInformationSystem/MenuItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantInformationSystem
+{
+    public class MenuItem
+    {
+        private int _id;
+        private string _name;
+        private double _price;
+        private int _waitingTime;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public double Price { get => _price; set => _price = value; }
+        public int WaitingTime { get => _waitingTime; set => _waitingTime = value; }
+
+        public MenuItem(int id, string name, double price, int waitingTime)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            WaitingTime = waitingTime;
+        }
+    }
+}
